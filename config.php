4dab558a49cbe3334ec41569a8694ba57081fc1f
Bueltge <Bueltge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-12 17:52:31 +0000

    add new tag for release
    git-svn-id: https://plugins.svn.wordpress.org/adminer/tags/1.2.1@556818 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * If you use the plugins outside the wp-root or other one install, 
+ * then you must define the absolute path to wp-load.php in the install root of WordPress
+ */
+
+// the path to wp-load.php
+$wp_siteurl = 'C:/xampp/htdocs/wpbeta';
